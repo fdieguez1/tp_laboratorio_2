26e3751c756bdad8d3edf1373f74de73c0a11d89
Fernando Dieguez <=>
2021-10-03 19:30:27 -0300

    finalizado TP-02

---
+++ TP-02/Entidades/Vehiculo.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    /// <summary>
+    /// La clase Vehiculo no deberá permitir que se instancien elementos de este tipo.
+    /// </summary>
+#pragma warning disable CS0660 // Type defines operator == or operator != but does not override Object.Equals(object o)
+#pragma warning disable CS0661 // Type defines operator == or operator != but does not override Object.GetHashCode()
+    public abstract class Vehiculo
+#pragma warning restore CS0661 // Type defines operator == or operator != but does not override Object.GetHashCode()
+#pragma warning restore CS0660 // Type defines operator == or operator != but does not override Object.Equals(object o)
+    {
+        public enum EMarca
+        {
+            Chevrolet, Ford, Renault, Toyota, BMW, Honda, HarleyDavidson
+        }
+        public enum ETamanio
+        {
+            Chico,
+            Mediano,
+            Grande
+        }
+
+        string chasis;
+        ConsoleColor color;
+        EMarca marca;
+
+        /// <summary>
+        /// ReadOnly: Retornará el tamaño
+        /// </summary>
+        protected abstract ETamanio Tamanio { get; set; }
+
+        /// <summary>
+        /// Publica todos los datos del Vehiculo.
+        /// </summary>
+        /// <returns></returns>
+        public virtual string Mostrar()
+        {
+            return (string)this;
+        }
+
+        /// <summary>
+        /// Devuelve un string con los datos de un vehiculo
+        /// </summary>
+        /// <param name="p">p Vehiculo a mostrar</param>
+        public static explicit operator string(Vehiculo p)
+        {
+            StringBuilder sb = new StringBuilder();
+            string typeCapital = p.GetType().Name.ToUpper();
+            sb.AppendLine($"{typeCapital}\r");
+            sb.AppendLine($"CHASIS: {p.chasis}\r");
+            sb.AppendLine($"MARCA : {p.marca.ToString()}\r");
+            sb.AppendLine($"COLOR : {p.color.ToString()}\r");
+            sb.AppendLine($"---------------------\n");
+            sb.Append($"TAMAÑO : {p.Tamanio}");
+            return sb.ToString();
+        }
+
+        /// <summary>
+        /// Dos vehiculos son iguales si comparten el mismo chasis
+        /// </summary>
+        /// <param name="v1"></param>
+        /// <param name="v2"></param>
+        /// <returns></returns>
+        public static bool operator ==(Vehiculo v1, Vehiculo v2)
+        {
+            return (v1.chasis == v2.chasis);
+        }
+        /// <summary>
+        /// Dos vehiculos son distintos si su chasis es distinto
+        /// </summary>
+        /// <param name="v1"></param>
+        /// <param name="v2"></param>
+        /// <returns></returns>
+        public static bool operator !=(Vehiculo v1, Vehiculo v2)
+        {
+            return (v1.chasis == v2.chasis);
+        }
+
+        
+        public Vehiculo(string chasis, EMarca marca, ConsoleColor color)
+        {
+            this.chasis = chasis;
+            this.marca = marca;
+            this.color = color;
+        }
+    }
+}
